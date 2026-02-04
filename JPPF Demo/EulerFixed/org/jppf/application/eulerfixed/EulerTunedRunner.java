544f177b732bc87945ce44007cd226feebf759ed
Quinn <kyled073@gmail.com>
2026-02-04 12:39:47 +0000

    Load Balance Res

--- JPPF Demo/EulerFixed/org/jppf/application/eulerfixed/EulerTunedRunner.java
+++ JPPF Demo/EulerFixed/org/jppf/application/eulerfixed/EulerTunedRunner.java
@@ -16,51 +16,77 @@ public class EulerTunedRunner {
 	
 	static long start;
 	static long finish;
-	long timeElapsed;
-	long[] timeElapseCollection;
-	final static int EULER_TARGET = 25000;
+	final static int EULER_TARGET = 20000;
 	final static int MAX_JOBS = 16;
-	final static int ITERATION_COUNT = 1;
+	final static int ITERATION_COUNT = 10;
 	final static int MIN_TASK_RANGE = 1;
 	
-	public static void main(final String...args) {
-		long timeElapsed = 0; //TODO: UN-INIT
+	enum EulerConfig {
+		Tuned("EulerTuned") {
+			@Override
+			public long eulerSum(JPPFClient jppfClient, int eulerTarget) {
+				return createTunedEulerJobList(jppfClient, EULER_TARGET);
+			}
+			
+		},
+		Naive("EulerNaive") {
+			@Override
+			public long eulerSum(JPPFClient jppfClient, int eulerTarget) {
+				return createNaiveEulerJobList(jppfClient, EULER_TARGET);
+			}
+			
+		};
 		
-		try (final JPPFClient jppfClient = new JPPFClient()) {
-			final EulerTunedRunner runner = new EulerTunedRunner();
-//			try (BufferedWriter writer = new BufferedWriter(new FileWriter("results.txt"))) {
-//				
-//				// Experiment
-//				for(int jobs = 1; jobs <= MAX_JOBS; jobs++) {
-//					writer.write("Job Count: " + jobs + "\n");
-					for(int i = 0; i < ITERATION_COUNT; i++) {
-						
-						// run experiment
-						createTunedEulerJobList(jppfClient, EULER_TARGET);
-						
-//						// write results
-//						System.out.println("Iteration " + i + ": " + timeElapsed + "ms.");
-//						writer.write("Iteration[" + i +"] Elapsed Time: " + timeElapsed + "ms\n");
-					}
-//					writer.write("\n"); 
-//				}
-//				writer.close();
-//			}
-//		} catch(final Exception e) {
-//			e.printStackTrace();
+		private String name;
+		
+		private EulerConfig(String name) {
+			this.name = name;
+		}
+		
+		public abstract long eulerSum(JPPFClient jppfClient, int eulerTarget);
+		
+		public String getName() {
+			return name;
 		}
 	}
 	
-	public static void createTunedEulerJobList(final JPPFClient jppfClient, int eulerTarget) {
+	public static void main(final String...args) {
+		long timeElapsed;
+		
+		try (final JPPFClient jppfClient = new JPPFClient()) {
+			
+			for(EulerConfig euler : EulerConfig.values()) {
+				try (BufferedWriter writer = new BufferedWriter(new FileWriter("Results/Nodes/Node1/"+ euler.getName() + "results.txt"))) {
+					
+						// Experiment
+						for(int i = 0; i < ITERATION_COUNT; i++) {
+							// Tuned Run
+							timeElapsed = euler.eulerSum(jppfClient, EULER_TARGET);
+							
+							// write results
+							System.out.println("Iteration " + i + ": " + timeElapsed + "ms.");
+							writer.write("Iteration[" + i +"] Elapsed Time: " + timeElapsed + "ms\n");
+						}
+						writer.write("\n"); 
+						writer.close();
+					}
+			}
+		} catch(final Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static long createTunedEulerJobList(final JPPFClient jppfClient, int eulerTarget) {
 		int power = 1;
-		int range = (int) (eulerTarget / Math.pow(2, power));
+		int range = ((int) (eulerTarget / Math.pow(2, power)));
 		int lower = 0;
 		int higher = lower + range;
 		
 		final List<JPPFJob> jobList = new ArrayList<>();
+		start = System.nanoTime();
 		while(range > MIN_TASK_RANGE) {
 			System.out.println("Euler Job " + power + ": [" + lower + " , " + higher + "], range: " + range);
-			JPPFJob newJob = createTunedEulerJob(jppfClient, lower, higher);
+			JPPFJob newJob = createEulerJob(jppfClient, lower, higher);
 			jppfClient.submitAsync(newJob);
 			jobList.add(newJob);
 			
@@ -71,17 +97,9 @@ public class EulerTunedRunner {
 		
 		for(int i = lower; i < eulerTarget; i++) {
 			System.out.println("Euler Job: [" + i + " , " + (i+1) + "], range: " + MIN_TASK_RANGE);
-			JPPFJob newJob = createTunedEulerJob(jppfClient, i, i+1);
+			JPPFJob newJob = createEulerJob(jppfClient, i, i+1);
 			jppfClient.submitAsync(newJob);
 			jobList.add(newJob);
-		}
-		
-		
-		try {
-			System.out.println("Job Count: " + jobList.size());
-			ensureNumberOfConnections(jppfClient, jobList.size());
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 		
 		int sumResult = 1;
@@ -90,10 +108,52 @@ public class EulerTunedRunner {
 	      sumResult += processExecutionResults(results);
 	    }
 	    
+	    finish = System.nanoTime();
 	    System.out.println("SumEuler("+eulerTarget+"): " + sumResult);
+	    
+	    return (finish - start) / 1000000;
 	}
 	
-	public static JPPFJob createTunedEulerJob(final JPPFClient jppfClient, int lower, int higher){
+	public static long createNaiveEulerJobList(final JPPFClient jppfClient, int eulerTarget) {
+		try {
+			ensureNumberOfConnections(jppfClient, MAX_JOBS);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		int range = eulerTarget / MAX_JOBS;
+		int lower = 0;
+		int higher = lower + range;
+		
+		final List<JPPFJob> jobList = new ArrayList<>(MAX_JOBS);
+		start = System.nanoTime();
+		while(higher < eulerTarget) {
+			System.out.println("Euler Job: [" + lower + " , " + higher + "], range: " + range);
+			JPPFJob newJob = createEulerJob(jppfClient, lower, higher);
+			jppfClient.submitAsync(newJob);
+			jobList.add(newJob);
+			lower = higher;
+			higher = higher + range;
+		}
+		
+		if(higher >= eulerTarget) {
+			System.out.println("Euler Job: [" + lower + " , " + eulerTarget + "], range: " + (eulerTarget - lower));
+			JPPFJob newJob = createEulerJob(jppfClient, lower, eulerTarget);
+			jppfClient.submitAsync(newJob);
+			jobList.add(newJob);
+		}
+		
+		int sumResult = 1;
+	    for (final JPPFJob job: jobList) {
+	      final List<Task<?>> results = job.awaitResults();
+	      sumResult += processExecutionResults(results);
+	    }
+	    
+	    finish = System.nanoTime();
+	    System.out.println("SumEuler("+eulerTarget+"): " + sumResult);
+	    return (finish - start) / 1000000;
+	}
+	
+	public static JPPFJob createEulerJob(final JPPFClient jppfClient, int lower, int higher){
 		JPPFJob job = new JPPFJob();
 		job.setName("EulerSum["+lower+","+higher+"]");
 		
